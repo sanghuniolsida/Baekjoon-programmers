f9224c88630bc0844355d2965d64f62f12778866
sanghuniolsida <168408335+sanghuniolsida@users.noreply.github.com>
2024-08-11 17:03:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 408 ms, Memory: 86336 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -7,7 +7,8 @@ public class Main {
         int N = sc.nextInt();
         int i = 1;
         int i_num = 666;
-
+        
+        // .contains("666") -> 문자열에 내가 찾고자 하는게 있는지 확인하는 메소드
         while(i<N) {
             i_num++;
             String str1 = String.valueOf(i_num);
