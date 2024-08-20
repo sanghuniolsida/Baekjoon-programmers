34cbd0ca4205d41820cf4da73df3189279cd9687
sanghuniolsida <168408335+sanghuniolsida@users.noreply.github.com>
2024-08-20 23:22:14 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14356 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,20 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split("");
-
-        int r=31;
-        int M=1234567891;
-        int[] int_arr = new int[L];
-        for(int i=0;i<L;i++){
-            int_arr[i] = arr[i].charAt(0)-96;
-        }
+        char[] arr = br.readLine().toCharArray();
+        
+        int r = 31;
+        int M = 1234567891;
+        
         long result = 0;
-
-        for(int i=0;i<int_arr.length;i++) {
-            long power = (long)Math.pow(r,i) % M;
-            result = (result + (power * int_arr[i]) % M) % M;
+        long rPower = 1; // r^0
+        
+        for(int i = 0; i < L; i++) {
+            int charValue = arr[i] - 'a' + 1; // 'a' -> 1, 'b' -> 2, ...
+            result = (result + (charValue * rPower) % M) % M;
+            rPower = (rPower * r) % M; // r^i 를 계산하며, 매번 M으로 나눈 나머지를 저장
         }
+        
         System.out.println(result);
     }
 }
