9a126942ffed756814118f04cadfae0c4ab8c2ce
sanghuniolsida <168408335+sanghuniolsida@users.noreply.github.com>
2025-02-20 00:08:35 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,37 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        int N = Integer.parseInt(arr[0]);
-        int K = Integer.parseInt(arr[1]);
-        int[] arr2 = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
         int count = 0;
-        for(int i = 0; i < N; i++) {
-            arr2[i] = Integer.parseInt(br.readLine());
-            if(arr2[i] <= K) {
-                count++;
-            }
-        }
-        int[] arr3 = new int[count];
-        for(int i = 0; i < count; i++) {
-            arr3[i] = arr2[i];
-        }
-        int cnt = 0;
-        int result = K;
-        int index = count - 1;
-        while(result != 0) {
-            if(result >= arr3[index]) {
-                result -= arr3[index];
-                cnt++;
-            } else {
-                index--;
+        for(int i=N-1;i>=0;i--) {
+            if(K / arr[i] > 0) {
+                count += K / arr[i];
+                K = K % arr[i];
+                if(K == 0) {
+                    break;
                 }
             }
-        System.out.println(cnt);
         }
+        System.out.println(count);
     }
+}
