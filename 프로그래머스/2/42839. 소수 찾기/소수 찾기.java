56a2a33903da157a3aaf818cbd74107abae830b1
SANGHUN PARK <168408335+sanghuniolsida@users.noreply.github.com>
2026-05-05 13:44:54 +0900

    [level 2] Title: 소수 찾기, Time: 104.56 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -31,7 +31,7 @@ class Solution {
     public boolean isPrime(int num) {
         if (num < 2) return false;
 
-        for (int i = 2; i * i <= num; i++) {
+        for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
             }
