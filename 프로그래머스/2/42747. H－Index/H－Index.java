32d8f00c64a7a5c82d5b06c035fb72f291eb145d
SANGHUN PARK <168408335+sanghuniolsida@users.noreply.github.com>
2026-05-05 12:26:05 +0900

    [level 2] Title: H-Index, Time: 0.64 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        
+        int len = citations.length;
+        for(int i=0;i<len;i++) {
+            if(citations[i] >= len - i) {
+                answer = len - i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
