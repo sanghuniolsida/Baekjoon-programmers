ac17b4b364fe1949deaccf2e4ed86c2cd0ead9c0
SANGHUN PARK <168408335+sanghuniolsida@users.noreply.github.com>
2026-05-05 13:43:50 +0900

    [level 2] Title: 소수 찾기, Time: 25.57 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    boolean[] visited;
+    public int solution(String numbers) {
+        int answer = 0;
+        visited = new boolean[numbers.length()];
+        
+        dfs("", numbers);
+        for(int num : set) {
+            if(isPrime(num)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public void dfs(String current, String numbers) {
+        if(!current.equals("")) {
+            set.add(Integer.parseInt(current));
+        }
+        for(int i=0;i<numbers.length();i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(current + numbers.charAt(i), numbers);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
