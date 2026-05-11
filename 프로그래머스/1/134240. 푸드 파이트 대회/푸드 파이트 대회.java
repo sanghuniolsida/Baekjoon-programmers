38571601ef247b163b5367181ddca58694f621e1
SANGHUN PARK <168408335+sanghuniolsida@users.noreply.github.com>
2026-05-11 19:56:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 10.33 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,21 +2,18 @@ import java.util.*;
 class Solution {
     public String solution(int[] food) {
         String answer = "";
-        Stack<String> stack = new Stack<>();
         for(int i=1;i<food.length;i++) {
             int cnt = food[i] / 2;
             String str = String.valueOf(i);
             for(int j=0;j<cnt;j++) {
                 answer += str;
-                stack.push(str);
             }
         }
         
-        answer += "0";
-        while(!stack.isEmpty()) {
-            String str2 = stack.pop();
-            answer += str2;
-        }
+        StringBuilder sb = new StringBuilder(answer);
+        String result = sb.reverse().toString();
+        answer = answer + "0" + result;
+        
         return answer;
     }
 }
